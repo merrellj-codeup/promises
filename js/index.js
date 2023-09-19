6994db13c80440f70cb27db07e8fe081cb620479
SgtMerrell <74498850+SgtMerrell@users.noreply.github.com>
2023-09-19 15:58:43 -0500

    step 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
+const getPerson = (id) => {
+	const url = `https://swapi.dev/api/people/${id}`;
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return fetch(url, options).then((response) => response.json());
+};
+
 //////// MAIN METHOD
 (async () => {
 	/////
+	getPerson(3).then((data) => {
+		console.log(data);
+	});
 })();
